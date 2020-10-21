3b3f8bbf50cad088a74ce1e33a246f8ea7fae5e5
Angel <a.demerdzhiev@gmail.com>
2020-10-21 14:49:35 +0300

    landing page project initial commit

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 document.addEventListener('click', function () {
   console.log("click, click");
 })
+
+let startingTime = performance.now();
+
+for (let i = 0; i < 100; i++) {
+  for (let j = 0; j < 100; j++) {
+    console.log('i and j are: ' , i , j);
+  }
+}
+
+let endingTime = performance.now();
+
+console.log('This code took ' + (endingTime - startingTime) + ' miliseconds to run');
